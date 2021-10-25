b0858a27bd23236a25cf63c51f88275c53568509
AlicjaOsam <58014617+AlicjaOsam@users.noreply.github.com>
2021-10-25 21:48:06 +0200

    Add files via upload

--- lab1/lab00.java
+++ lab1/lab00.java
@@ -8,17 +8,11 @@ import java.lang.Math.*;
 
 public class lab00{
     public static void main(String[] args){
-	/**Liczba a*/
         double a = 1.7;
-	/**Liczba b*/
         double b = 1.2;
-	/**Liczba h*/
         double h = 1.3;
-	/**Liczba t*/
         double t = 1.4;
-	/**Liczba x*/
         double x;
-	/**Liczba y*/
         double y;
         for(t=1.4; t<29.65; t=t+0.005){
             x = (a-b)*Math.cos(t)+h*Math.cos((a-b)/b*t);
